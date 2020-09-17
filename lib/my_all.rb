ebec3434b1da95e63f6e5c955185dc130269deff
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-17 21:59:57 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i= 0
+  while i < collection.length
+    all_bigger = yield(collection[i])
+  end
+  if all_bigger == false 
+    return false
+  end
+   #binding.pry
 end
+
+
+ 
+
+ 
